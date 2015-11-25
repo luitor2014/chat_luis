b9c2fe3ee6ed57bcada5fccbf59cce80e4ef99b1
luitor2014 <luisxxd_16@hotmail.com>
2015-11-25 17:48:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var cons= require('consolidate');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 app.engine('.jade',cons.jade);
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
