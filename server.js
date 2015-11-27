4abd77dc35eb756bf3a7dea7d11ce5a0a124dad3
luitor2014 <luisxxd_16@hotmail.com>
2015-11-26 22:45:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var cons= require('consolidate');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var PORT = process.env.PORT;
+var PORT = 51123;
 //var PORT = process.env.PORT;
 app.engine('.jade',cons.jade);
 app.set('view engine', 'jade');
